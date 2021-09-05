fd9109182734dbf0eb297cd8ed0a059f3b05d791
Beomsun <beomsun22@gmail.com>
2021-09-05 19:59:53 +0900

    hello querydsl

---
+++ Spring/hello-querydsl/src/test/java/com/bs/helloquerydsl/HelloQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.helloquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
