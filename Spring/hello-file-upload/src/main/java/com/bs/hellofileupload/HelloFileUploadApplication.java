0bb7d5e8255c60ad1ea67b3a2f4f709f027c264d
Beomsun <beomsun22@gmail.com>
2021-09-20 16:52:27 +0900

    hello file upload

---
+++ Spring/hello-file-upload/src/main/java/com/bs/hellofileupload/HelloFileUploadApplication.java
@@ -0,0 +1,15 @@
+package com.bs.hellofileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class HelloFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloFileUploadApplication.class, args);
+	}
+
+}
---
+++ Spring/hello-file-upload/src/test/java/com/bs/hellofileupload/HelloFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.hellofileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
