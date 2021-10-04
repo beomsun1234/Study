d3226785d9d2d82a7f91709336cc02852e0596e7
Beomsun <beomsun22@gmail.com>
2021-10-04 10:42:25 +0900

    hello-docker-compose

---
+++ Spring/hello-docker-compose/src/main/java/com/bs/hellodockercompose/HelloDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.bs.hellodockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDockerComposeApplication.class, args);
+	}
+
+}
---
+++ Spring/hello-docker-compose/src/test/java/com/bs/hellodockercompose/HelloDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.hellodockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
